40de2d0c97b07961ed239b5f2ff2111d50947323
anil vallala <anilvallala85@gmail.com>
2022-10-17 23:39:00 +0530

    dummy push3

--- src/main/java/com/docker/example/Application.java
+++ src/main/java/com/docker/example/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String args[]){
+        //dummy comment
         SpringApplication.run(Application.class);
         System.out.println("***Application Started***");
     }
